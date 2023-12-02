1a568ce9b6daffe19c271b7dd34c2d05b50715ed
torikallcode <torikallcode@gmail.com>
2023-12-03 07:55:16 +0800

    commit fry

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "Aaaaa, I like you too";
+  question.innerHTML = "I like you too Anak bayiii!!";
   gif.src =
     "https://raw.githubusercontent.com/DzarelDeveloper/Img/main/gif.webp";
 });
